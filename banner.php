008bb4c263d465f5071f93f608c03c72863d40c1
Dane Odekirk <dizzzy@uw.edu>
2011-10-07 14:02:56 -0700

    Added functional featured image

--- banner.php
+++ banner.php
@@ -1,5 +1,27 @@
-<div class="title"><h2><?php bloginfo('name'); ?></h2></div>
-<a href="<?php bloginfo('url'); ?>">
-    <!--img class="banner" src="<?php bloginfo('template_url'); ?>/images/banner.jpg" width="874" height="85" alt="Banner graphic" /-->
-	<img class="banner" src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="Banner graphic" />
-</a>
+<div class="title">
+    <h2><?php bloginfo('name'); ?></h2>
+</div>
+
+  <?php
+    // Check to see if the header image has been removed
+    $header_image = get_header_image();
+    if ( ! empty( $header_image ) ) :
+  ?>
+
+  <a href="<?php bloginfo('url'); ?>">
+
+      <?php
+        // The header image
+        // Check if this is a post or page, if it has a thumbnail, and if it's a big one
+        if ( is_singular() &&
+            has_post_thumbnail( $post->ID ) &&
+            ( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), array( HEADER_IMAGE_WIDTH, HEADER_IMAGE_WIDTH ) ) ) &&
+            $image[1] >= HEADER_IMAGE_WIDTH ) :
+          // Houston, we have a new header image!
+          echo get_the_post_thumbnail( $post->ID, 'post-thumbnail', array('class' => 'banner') );
+        else : ?>
+        <img class="banner" src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="Banner graphic" />
+      <?php endif; ?>
+
+  </a>
+<?php endif; ?>
