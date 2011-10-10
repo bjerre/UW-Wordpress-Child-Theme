95245a1cb19db9f4dc9cf721292de66f80b460c9
Dane Odekirk <dizzzy@uw.edu>
2011-10-10 15:00:37 -0700

    removing merge conflicts with uwadv

--- leftnav.php
+++ leftnav.php
@@ -1,30 +1,7 @@
   <div id="leftNav">
     <div class="leftNavBackground">
         
-        <?php
-          global $post;
-          
-          $args = array(
-            'depth'        => $post->post_parent ? 1 : 0,
-            'show_date'    => '',
-            'date_format'  => get_option('date_format'),
-            'child_of'     => $post->ID,
-            'exclude'      => '',
-            'include'      => '',
-            'title_li'     => get_the_title($post->post_parent),
-            'echo'         => 0,
-            'authors'      => '',
-            'sort_column'  => 'menu_order, post_title',
-            'link_before'  => '',
-            'link_after'   => '',
-            'walker'       => '' );
-          $menu = wp_list_pages($args);
-          if ( !strlen($menu)) {
-            wp_nav_menu('depth=1');
-          } else {
-            echo $menu;
-          }
-        ?>
+      <?php wp_nav_menu('depth=3'); ?>
 
       <br class="clear" /><br class="clear" />
     </div>
