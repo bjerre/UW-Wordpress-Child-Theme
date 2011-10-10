ab2919b69c4c5cb2202404cca087fa1df0b30864
Dane Odekirk <dizzzy@uw.edu>
2011-10-10 14:58:50 -0700

    possible left nav

--- leftnav.php
+++ leftnav.php
@@ -1,7 +1,30 @@
   <div id="leftNav">
     <div class="leftNavBackground">
         
-      <?php wp_nav_menu('depth=3'); ?>
+        <?php
+          global $post;
+          
+          $args = array(
+            'depth'        => $post->post_parent ? 1 : 0,
+            'show_date'    => '',
+            'date_format'  => get_option('date_format'),
+            'child_of'     => $post->ID,
+            'exclude'      => '',
+            'include'      => '',
+            'title_li'     => get_the_title($post->post_parent),
+            'echo'         => 0,
+            'authors'      => '',
+            'sort_column'  => 'menu_order, post_title',
+            'link_before'  => '',
+            'link_after'   => '',
+            'walker'       => '' );
+          $menu = wp_list_pages($args);
+          if ( !strlen($menu)) {
+            wp_nav_menu('depth=1');
+          } else {
+            echo $menu;
+          }
+        ?>
 
       <br class="clear" /><br class="clear" />
     </div>
