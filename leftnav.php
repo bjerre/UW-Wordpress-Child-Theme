329f8dd15ebad6d2552c2436c6e4ce14c6b168c9
Dane Odekirk <dizzzy@uw.edu>
2011-10-11 07:40:06 -0700

    css changes and leftnav depth changed to 2

--- leftnav.php
+++ leftnav.php
@@ -1,7 +1,7 @@
   <div id="leftNav">
     <div class="leftNavBackground">
         
-      <?php wp_nav_menu('depth=3'); ?>
+      <?php wp_nav_menu('depth=2'); ?>
 
       <br class="clear" /><br class="clear" />
     </div>
