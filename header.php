40d8c1303a9aee4201695501d110a94801e8640e
Dane Odekirk <dizzzy@uw.edu>
2011-10-11 14:34:53 -0700

    header bug fix

--- header.php
+++ header.php
@@ -1,14 +1,4 @@
-<?php
-/**
- * The Header for our theme.
- *
- * Displays all of the <head> section and everything up till <div id="main">
- *
- * @package WordPress
- * @subpackage Twenty_Eleven
- * @since Twenty Eleven 1.0
- */
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <!--[if IE 6]>
 <html id="ie6" <?php language_attributes(); ?>>
 <![endif]-->
