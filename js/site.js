7aef2954a1685096ada0b58df7ad4e1489869de2
Dane Odekirk <dizzzy@uw.edu>
2011-10-12 10:19:23 -0700

    bug fix

--- js/site.js
+++ js/site.js
@@ -17,7 +17,7 @@ jQuery(document).ready(function($){
             $('.banner').append(new_img)
               .children('img').first()
               .fadeOut(200, function() {
-                $(this).first().remove();
+                $(this).remove();
               });
             $(this).fadeIn(200);
           });
