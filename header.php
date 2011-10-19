1016f6714974ed796e51b900eb5aa67b6a8fe063
Dane Odekirk <dizzzy@uw.edu>
2011-10-19 09:42:11 -0700

    html template fix

--- header.php
+++ header.php
@@ -56,26 +56,6 @@
 	wp_head();
 ?>
 </head>
-
-<!-- The line below starts the conditional comment -->
-<!--[if IE]>
-<style type="text/css">
-  body{ behavior:url("<?php bloginfo('template_directory'); ?>csshover.htc"); }
-</style>
-<![endif]--> <!-- This ends the conditional comment -->
-    
-<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
-<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
-<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
-<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
-<?php wp_get_archives('type=monthly&format=link'); ?>
-    
-<style type="text/css" media="screen">
-<!-- @import url( <?php bloginfo('stylesheet_url'); ?> ); -->
-</style>
-
-  </head>
   <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 
       <body>
